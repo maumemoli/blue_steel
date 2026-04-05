c280169685c8c7dc69c01b8e2541b5d7489aeb94
DoubleM <mau.memoli@gmail.com>
2026-04-05 23:00:16 +0700

    Fixed some bugs on the shelf and the init not working in maya 2023

--- releases/maya/BlueSteel/scripts/blue_steel/__init__.py
+++ releases/maya/BlueSteel/scripts/blue_steel/__init__.py
@@ -1,6 +1,10 @@
 from . import env
-import requests
-print("BlueSteel initialized on maya {} on python".format(env.MAYA_VERSION, env.PYTHON_VERSION))
+try:
+    import requests
+except ImportError:
+    print("The 'requests' library is not installed. Please install it to enable update checking.")
+    requests = None
+print("BlueSteel initialized on maya {} on python {}".format(env.MAYA_VERSION, env.PYTHON_VERSION))
 if env.MAYA_VERSION < 2022 or env.PYTHON_VERSION < 3:
     raise RuntimeError("BlueSteel requires Maya 2022 or higher with Python 3.x")
 __version__ = env.VERSION
